383f0c67bf4403ce19e520a9816b1c4d23be0ea4
Unknown <tildenwinston@gmail.com>
2018-03-26 03:35:47 -0400

    Back to layout without scroll bars

--- src/view1.java
+++ src/view1.java
@@ -20,11 +20,6 @@ import java.awt.Window.Type;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JDesktopPane;
-<<<<<<< HEAD
-import javax.swing.JScrollPane;
-import javax.swing.SwingConstants;
-=======
->>>>>>> 1045bd8... Visually there. Before Testing Scroll Bars
 
 public class view1 {
 
@@ -105,131 +100,21 @@ public class view1 {
 	 */
 	private void initialize() {
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		double width = screenSize.getWidth();
-		double height = screenSize.getHeight();
+		double width = (screenSize.getWidth()) * .8;
+		if (width < 1024) {
+			width = 1024;
+		}
+		
+		double height = (screenSize.getHeight()) * .8;
+		if (width < 768) {
+			width = 768;
+		}
+		
 		frame = new JFrame();
-		frame.setBounds(100, 100, (int)(width*.6), (int)(height*.8));
+		frame.setBounds(100, 100, (int)(width), (int)(height));
 		
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-<<<<<<< HEAD
-		frame.getContentPane().setLayout(null);
-		
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(7, 11, (int)((width*.6)-20), (int)((height*.8)-20));
-		frame.getContentPane().add(scrollPane);
-		
-		JPanel panel = new JPanel();
-		scrollPane.setViewportView(panel);
-		panel.setLayout(new MigLayout("", "[15px:n:30px,grow,center][120px:126.00px,grow][90px:n,grow][80px:n,grow][115px:n,grow][::90px,grow]", "[44.00px][][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][]"));
-		
-//		JLabel lblNewLabel = new JLabel("GPA Calculator");
-//		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-//		panel.add(lblNewLabel, "cell 0 0,alignx left,aligny top");
-//		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		
-		JLabel lblNewLabel = new JLabel("GPA Calculator");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		panel.add(lblNewLabel, "cell 1 0,alignx center,aligny top");
-		
-		JButton button = new JButton("HELP");
-		panel.add(button, "cell 5 0,alignx center");
-		
-		JLabel lblNewLabel_1 = new JLabel("Credit Hours");
-		panel.add(lblNewLabel_1, "cell 2 1");
-		
-		JLabel lblNewLabel_2 = new JLabel("Grade (Optional)");
-		panel.add(lblNewLabel_2, "cell 3 1");
-		
-		JLabel lblNewLabel_3 = new JLabel("Course Name (Optional)");
-		panel.add(lblNewLabel_3, "cell 4 1");
-		
-		JButton btnDeleteAll = new JButton("Delete All");
-		panel.add(btnDeleteAll, "cell 5 1");
-		
-		JLabel label = new JLabel("1");
-		panel.add(label, "cell 0 2,alignx center");
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"taken", "current", "anticipated"}));
-		panel.add(comboBox, "cell 1 2,growx");
-		
-		creditHours1 = new JTextField();
-		panel.add(creditHours1, "cell 2 2,growx");
-		creditHours1.setColumns(10);
-		
-		grade1 = new JTextField();
-		panel.add(grade1, "cell 3 2,growx");
-		grade1.setColumns(10);
-		
-		courseName1 = new JTextField();
-		panel.add(courseName1, "cell 4 2,growx");
-		courseName1.setColumns(10);
-		
-		JButton btnNewButton = new JButton("Delete Row");
-		panel.add(btnNewButton, "cell 5 2");
-		
-		JLabel label_1 = new JLabel("2");
-		panel.add(label_1, "cell 0 3,alignx center");
-		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"taken", "current", "anticipated"}));
-		panel.add(comboBox_1, "cell 1 3,growx");
-		
-		textField = new JTextField();
-		textField.setColumns(10);
-		panel.add(textField, "cell 2 3,growx");
-		
-		textField_14 = new JTextField();
-		textField_14.setColumns(10);
-		panel.add(textField_14, "cell 3 3,growx");
-		
-		textField_17 = new JTextField();
-		textField_17.setColumns(10);
-		panel.add(textField_17, "cell 4 3,growx");
-		
-		JLabel label_2 = new JLabel("3");
-		panel.add(label_2, "cell 0 4,alignx center");
-		
-		JComboBox comboBox_2 = new JComboBox();
-		panel.add(comboBox_2, "cell 1 4,growx");
-		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		panel.add(textField_1, "cell 2 4,growx");
-		
-		textField_15 = new JTextField();
-		textField_15.setColumns(10);
-		panel.add(textField_15, "cell 3 4,growx");
-		
-		JLabel label_3 = new JLabel("4");
-		panel.add(label_3, "cell 0 5,alignx center");
-		
-		JComboBox comboBox_3 = new JComboBox();
-		panel.add(comboBox_3, "cell 1 5,growx");
-		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		panel.add(textField_2, "cell 2 5,growx");
-		
-		textField_16 = new JTextField();
-		textField_16.setColumns(10);
-		panel.add(textField_16, "cell 3 5,growx");
-		
-		JLabel label_4 = new JLabel("5");
-		panel.add(label_4, "cell 0 6,alignx center");
-		
-		JComboBox comboBox_4 = new JComboBox();
-		panel.add(comboBox_4, "cell 1 6,growx");
-		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		panel.add(textField_3, "cell 2 6,growx");
-		
-		textField_18 = new JTextField();
-		panel.add(textField_18, "cell 3 6,growx");
-=======
 		frame.getContentPane().setLayout(new MigLayout("", "[0px:n,grow][15px:n:30px,grow,center][120px:126.00px:150px,grow][90px:n:100px,grow][155px:n:180px,grow][115px:n:175px,grow][::90px,grow][0px:n,grow]", "[44.00px][][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][::100px,grow][][]"));
 		
 		JLabel lblNewLabel = new JLabel("GPA Calculator");
@@ -349,7 +234,6 @@ public class view1 {
 		
 		textField_18 = new JTextField();
 		frame.getContentPane().add(textField_18, "cell 4 6,growx");
->>>>>>> 1045bd8... Visually there. Before Testing Scroll Bars
 		textField_18.setColumns(10);
 		
 		textField_32 = new JTextField();
@@ -360,19 +244,6 @@ public class view1 {
 		frame.getContentPane().add(button_5, "cell 6 6");
 		
 		JLabel label_5 = new JLabel("6");
-<<<<<<< HEAD
-		panel.add(label_5, "cell 0 7,alignx center");
-		
-		JComboBox comboBox_5 = new JComboBox();
-		panel.add(comboBox_5, "cell 1 7,growx");
-		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		panel.add(textField_4, "cell 2 7,growx");
-		
-		textField_19 = new JTextField();
-		panel.add(textField_19, "cell 3 7,growx");
-=======
 		frame.getContentPane().add(label_5, "cell 1 7,alignx center");
 		
 		JComboBox comboBox_5 = new JComboBox();
@@ -384,7 +255,6 @@ public class view1 {
 		
 		textField_19 = new JTextField();
 		frame.getContentPane().add(textField_19, "cell 4 7,growx");
->>>>>>> 1045bd8... Visually there. Before Testing Scroll Bars
 		textField_19.setColumns(10);
 		
 		textField_33 = new JTextField();
@@ -395,19 +265,6 @@ public class view1 {
 		frame.getContentPane().add(button_6, "cell 6 7");
 		
 		JLabel label_6 = new JLabel("7");
-<<<<<<< HEAD
-		panel.add(label_6, "cell 0 8,alignx center");
-		
-		JComboBox comboBox_6 = new JComboBox();
-		panel.add(comboBox_6, "cell 1 8,growx");
-		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		panel.add(textField_5, "cell 2 8,growx");
-		
-		textField_20 = new JTextField();
-		panel.add(textField_20, "cell 3 8,growx");
-=======
 		frame.getContentPane().add(label_6, "cell 1 8,alignx center");
 		
 		JComboBox comboBox_6 = new JComboBox();
@@ -419,7 +276,6 @@ public class view1 {
 		
 		textField_20 = new JTextField();
 		frame.getContentPane().add(textField_20, "cell 4 8,growx");
->>>>>>> 1045bd8... Visually there. Before Testing Scroll Bars
 		textField_20.setColumns(10);
 		
 		textField_34 = new JTextField();
@@ -430,19 +286,6 @@ public class view1 {
 		frame.getContentPane().add(button_7, "cell 6 8");
 		
 		JLabel label_7 = new JLabel("8");
-<<<<<<< HEAD
-		panel.add(label_7, "cell 0 9,alignx center");
-		
-		JComboBox comboBox_7 = new JComboBox();
-		panel.add(comboBox_7, "cell 1 9,growx");
-		
-		textField_6 = new JTextField();
-		textField_6.setColumns(10);
-		panel.add(textField_6, "cell 2 9,growx");
-		
-		textField_21 = new JTextField();
-		panel.add(textField_21, "cell 3 9,growx");
-=======
 		frame.getContentPane().add(label_7, "cell 1 9,alignx center");
 		
 		JComboBox comboBox_7 = new JComboBox();
@@ -454,7 +297,6 @@ public class view1 {
 		
 		textField_21 = new JTextField();
 		frame.getContentPane().add(textField_21, "cell 4 9,growx");
->>>>>>> 1045bd8... Visually there. Before Testing Scroll Bars
 		textField_21.setColumns(10);
 		
 		textField_35 = new JTextField();
@@ -465,19 +307,6 @@ public class view1 {
 		frame.getContentPane().add(button_8, "cell 6 9");
 		
 		JLabel label_8 = new JLabel("9");
-<<<<<<< HEAD
-		panel.add(label_8, "cell 0 10,alignx center");
-		
-		JComboBox comboBox_8 = new JComboBox();
-		panel.add(comboBox_8, "cell 1 10,growx");
-		
-		textField_7 = new JTextField();
-		textField_7.setColumns(10);
-		panel.add(textField_7, "cell 2 10,growx");
-		
-		textField_22 = new JTextField();
-		panel.add(textField_22, "cell 3 10,growx");
-=======
 		frame.getContentPane().add(label_8, "cell 1 10,alignx center");
 		
 		JComboBox comboBox_8 = new JComboBox();
@@ -489,7 +318,6 @@ public class view1 {
 		
 		textField_22 = new JTextField();
 		frame.getContentPane().add(textField_22, "cell 4 10,growx");
->>>>>>> 1045bd8... Visually there. Before Testing Scroll Bars
 		textField_22.setColumns(10);
 		
 		textField_36 = new JTextField();
@@ -500,19 +328,6 @@ public class view1 {
 		frame.getContentPane().add(button_9, "cell 6 10");
 		
 		JLabel label_9 = new JLabel("10");
-<<<<<<< HEAD
-		panel.add(label_9, "cell 0 11,alignx center");
-		
-		JComboBox comboBox_9 = new JComboBox();
-		panel.add(comboBox_9, "cell 1 11,growx");
-		
-		textField_8 = new JTextField();
-		textField_8.setColumns(10);
-		panel.add(textField_8, "cell 2 11,growx");
-		
-		textField_23 = new JTextField();
-		panel.add(textField_23, "cell 3 11,growx");
-=======
 		frame.getContentPane().add(label_9, "cell 1 11,alignx center");
 		
 		JComboBox comboBox_9 = new JComboBox();
@@ -524,7 +339,6 @@ public class view1 {
 		
 		textField_23 = new JTextField();
 		frame.getContentPane().add(textField_23, "cell 4 11,growx");
->>>>>>> 1045bd8... Visually there. Before Testing Scroll Bars
 		textField_23.setColumns(10);
 		
 		textField_37 = new JTextField();
@@ -535,19 +349,6 @@ public class view1 {
 		frame.getContentPane().add(button_10, "cell 6 11");
 		
 		JLabel label_10 = new JLabel("11");
-<<<<<<< HEAD
-		panel.add(label_10, "cell 0 12,alignx center");
-		
-		JComboBox comboBox_10 = new JComboBox();
-		panel.add(comboBox_10, "cell 1 12,growx");
-		
-		textField_9 = new JTextField();
-		textField_9.setColumns(10);
-		panel.add(textField_9, "cell 2 12,growx");
-		
-		textField_24 = new JTextField();
-		panel.add(textField_24, "cell 3 12,growx");
-=======
 		frame.getContentPane().add(label_10, "cell 1 12,alignx center");
 		
 		JComboBox comboBox_10 = new JComboBox();
@@ -559,7 +360,6 @@ public class view1 {
 		
 		textField_24 = new JTextField();
 		frame.getContentPane().add(textField_24, "cell 4 12,growx");
->>>>>>> 1045bd8... Visually there. Before Testing Scroll Bars
 		textField_24.setColumns(10);
 		
 		textField_38 = new JTextField();
@@ -570,19 +370,6 @@ public class view1 {
 		frame.getContentPane().add(button_11, "cell 6 12");
 		
 		JLabel label_11 = new JLabel("12");
-<<<<<<< HEAD
-		panel.add(label_11, "cell 0 13,alignx center");
-		
-		JComboBox comboBox_11 = new JComboBox();
-		panel.add(comboBox_11, "cell 1 13,growx");
-		
-		textField_10 = new JTextField();
-		textField_10.setColumns(10);
-		panel.add(textField_10, "cell 2 13,growx");
-		
-		textField_25 = new JTextField();
-		panel.add(textField_25, "cell 3 13,growx");
-=======
 		frame.getContentPane().add(label_11, "cell 1 13,alignx center");
 		
 		JComboBox comboBox_11 = new JComboBox();
@@ -594,7 +381,6 @@ public class view1 {
 		
 		textField_25 = new JTextField();
 		frame.getContentPane().add(textField_25, "cell 4 13,growx");
->>>>>>> 1045bd8... Visually there. Before Testing Scroll Bars
 		textField_25.setColumns(10);
 		
 		textField_39 = new JTextField();
@@ -605,43 +391,6 @@ public class view1 {
 		frame.getContentPane().add(button_12, "cell 6 13");
 		
 		JLabel label_12 = new JLabel("13");
-<<<<<<< HEAD
-		panel.add(label_12, "cell 0 14,alignx center");
-		
-		JComboBox comboBox_12 = new JComboBox();
-		panel.add(comboBox_12, "cell 1 14,growx");
-		
-		textField_11 = new JTextField();
-		textField_11.setColumns(10);
-		panel.add(textField_11, "cell 2 14,growx");
-		
-		JLabel label_13 = new JLabel("14");
-		panel.add(label_13, "cell 0 15,alignx center");
-		
-		JComboBox comboBox_13 = new JComboBox();
-		panel.add(comboBox_13, "cell 1 15,growx");
-		
-		textField_12 = new JTextField();
-		textField_12.setColumns(10);
-		panel.add(textField_12, "cell 2 15,growx");
-		
-		JLabel label_14 = new JLabel("15");
-		panel.add(label_14, "cell 0 16,alignx center");
-		
-		JComboBox comboBox_14 = new JComboBox();
-		panel.add(comboBox_14, "cell 1 16,growx");
-		
-		textField_13 = new JTextField();
-		textField_13.setColumns(10);
-		panel.add(textField_13, "cell 2 16,growx");
-		
-		JButton btnCalculateGpa = new JButton("Calculate GPA");
-		panel.add(btnCalculateGpa, "cell 4 17,alignx center");
-		
-		JLabel GPA = new JLabel("GPA");
-		GPA.setFont(new Font("Tahoma", Font.BOLD, 11));
-		panel.add(GPA, "cell 5 17");
-=======
 		frame.getContentPane().add(label_12, "cell 1 14,alignx center");
 		
 		JComboBox comboBox_12 = new JComboBox();
@@ -726,7 +475,6 @@ public class view1 {
 		
 		JLabel lblNeededGpa = new JLabel("Needed GPA:");
 		frame.getContentPane().add(lblNeededGpa, "cell 6 18");
->>>>>>> 1045bd8... Visually there. Before Testing Scroll Bars
 		
 	}
 
